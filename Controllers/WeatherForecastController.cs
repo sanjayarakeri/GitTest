4b283be73c0f95e97087b0e118fd0aea41e4c34a
sanjayarakeri <sanjay05795@gmail.com>
2023-07-18 17:49:28 +0530

    Added NonAction method

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,8 +8,8 @@ namespace GitTest.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -29,5 +29,12 @@ namespace GitTest.Controllers
             })
             .ToArray();
         }
+
+        [NonAction]
+        public static void Test()
+        {
+            Console.WriteLine("Test Practice");
+        }
+       
     }
 }
